19837c1a7756f96d5edeb5959055d3b65e1c9475
Williamns <williamnstolb@gmail.com>
2022-05-07 11:51:33 -0300

    Página inicial do desafio 1: finalizada sem css

--- desafio-warren/src/pages/Desafio1.jsx
+++ desafio-warren/src/pages/Desafio1.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import desafio1 from '../utils/desafio1';
 
 function Desafio1() {
-  const list = desafio1(1000);
+  const [number, setNumber] = useState(0);
+  const list = desafio1(number);
+
+  const onChange = (e) => {
+    const value = e.target.value;
+    setNumber(value);
+  }
+
   return (
     <div>
       <h1>Desafio 1</h1>
+      <h4> Construa um algoritmo que mostre na tela todos os números n onde a soma de n + reverso(n) resultem em números ímpares que estão abaixo de 1 milhão.</h4>
       <div>
-        <input type="text" placeholder='Digite o número aqui'/>
+        <label htmlFor="number">Digite o número aqui { '===> ' } </label>
+        <input id="number" type="text" value={ number } onChange={ onChange } />
       </div>
       <div>
         <p >{ `Quantidade total de números: ${list.length}` } </p>
